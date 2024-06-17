3fcc46e1c15c316416a041a754fa19f56df2c91c
BCR-0632 <caiolopesfv@gmail.com>
2024-06-17 17:53:19 -0300

    Inserindo páginas iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import './App.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//pages
+import Home from './pages/home/home'
+import Admin from './pages/admin/admin'
+
 function App() {
 
   return <div className='App'>
+
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/admin' element={<Admin />}></Route>
+    </Routes>
+  </BrowserRouter>
 
   </div>
 }
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,13 @@
+
+import './admin.css'
+
+const Admin = () => {
+
+    return <div className='container admin-container'>
+
+
+    </div>
+
+}
+
+export default Admin;
