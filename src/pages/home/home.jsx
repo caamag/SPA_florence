ad30e6c462801de03df0c30ebb9095242773e7d5
Caio Lopes <caiolopesfv@gmail.com>
2024-06-18 20:49:19 -0300

    enviando atualizações

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
         <section className='section initial-section'>
             <div className='content initial-content'>
-                <h1>Histórias marcadas<br />na sua pele</h1>
+                <h1>Histórias que a <span>sua pele</span>  <br />pode contar.</h1>
                 <div>
                     <a href="" className='yellow-link'>Agendar horário</a>
                     <a href="">Explorar</a>
@@ -23,9 +23,22 @@ const Home = () => {
                         Independentemente do design, estamos prontos para transformar suas ideias em arte.</p><br /><br /><br />
                     <a href="">Explorar</a>
                 </div>
-                <div className="style-content"></div>
-                <div className="style-content"></div>
-                <div className="style-content"></div>
+                <a href=""><div className="style-content card realism-card"><h2>REALISMO</h2></div></a>
+                <a href=""><div className="style-content card cover-card"><h2>COBERTURA</h2></div></a>
+                <a href=""><div className="style-content card piercing-card"><h2>PIERCING</h2></div></a>
+            </div>
+        </section>
+
+        <section className='section space-section'>
+            <div className='content space-content'>
+                <div>
+                    <h2>Nosso espaço</h2>
+                    <p>Nosso estúdio é um espaço acolhedor e profissional, dedicado a criar tatuagens
+                        únicas e de alta qualidade. Com uma equipe de tatuadores talentosos e experientes,
+                        estamos prontos para trazer à vida os mais diversos estilos, desde os clássicos até
+                        os mais contemporâneos.</p>
+                </div>
+                <div></div>
             </div>
         </section>
 
