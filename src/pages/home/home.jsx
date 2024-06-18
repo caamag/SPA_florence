c052e3521726d47facdd7c574d0314d32d0041f4
Caio Lopes <caiolopesfv@gmail.com>
2024-06-18 15:40:58 -0300

    continuando home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -8,7 +8,24 @@ const Home = () => {
         <section className='section initial-section'>
             <div className='content initial-content'>
                 <h1>Histórias marcadas<br />na sua pele</h1>
-                <a href="">Agendar horário</a>
+                <div>
+                    <a href="" className='yellow-link'>Agendar horário</a>
+                    <a href="">Explorar</a>
+                </div>
+            </div>
+        </section>
+
+        <section className="section style-section">
+            <div className="content styles-container">
+                <div className="style-content">
+                    <h2>Estilo não<br /> é problema<br /> para nós.</h2><br /><br />
+                    <p>Criamos tatuagens personalizadas, perfeitamente alinhadas ao seu gosto<br /> e estilo.
+                        Independentemente do design, estamos prontos para transformar suas ideias em arte.</p><br /><br /><br />
+                    <a href="">Explorar</a>
+                </div>
+                <div className="style-content"></div>
+                <div className="style-content"></div>
+                <div className="style-content"></div>
             </div>
         </section>
 
