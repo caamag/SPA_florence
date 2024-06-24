a81d87f437161bc7f5d7f1d0fd98055d5756677c
Caio Lopes <caiolopesfv@gmail.com>
2024-06-24 20:40:43 -0300

    Enviando imagnes de marca e ataulizando nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,10 +1,16 @@
 
 import './nav.css'
+import { NavLink } from 'react-router-dom'
+
+//images
+import logo from '../../../public/logo_branco_florence.png'
 
 const Nav = () => {
 
     return <nav className='nav'>
-
+        <ul className='nav-items'>
+            <NavLink to={'/'}><img src={logo} alt="" className='logo-nav' /></NavLink>
+        </ul>
 
     </nav>
 
